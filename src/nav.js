73caef85b477e3e8c3d086505c8946bde930ea59
Evelyn Loosley-Millman <eloosleymillman@my.bcit.ca>
2025-11-10 16:31:51 -0800

    functional back arrow in footer, and working progress button in profile.html

---
+++ src/nav.js
@@ -0,0 +1,11 @@
+// nav.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const progressDiv = document.getElementById("progress");
+
+  if (progressDiv) {
+    progressDiv.addEventListener("click", () => {
+      window.location.href = "progress.html";
+    });
+  }
+});
