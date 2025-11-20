231e1d4247a4e58dd9d9fc970d9c1f076f08aafc
Danielle Laron <dlaron@my.bcit.ca>
2025-11-19 18:59:24 -0800

    fixed excess code from the addtask.js from previous work

--- src/addtask.js
+++ src/addtask.js
@@ -44,27 +44,4 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const goalName = input.value.trim();
-    //Don't let the user make a goal until they name the goal
-    if (!goalName) {
-      alert("Please enter a goal name.");
-      return;
-    }
-
-    //If successful, the goal is then added to a goals collection
-    //and re-direct to the taskpage.html. The goals made will then
-    //show up on the main task page
-    //Otherwise, if it doesn't work, catch the error and don't try
-    //anything else
-    try {
-      const goalsRef = collection(db, "goals");
-      await addDoc(goalsRef, { name: goalName });
-      window.location.href = "taskpage.html";
-    } catch (error) {
-      console.error("Error adding goal:", error);
-    }
-  });
 });
